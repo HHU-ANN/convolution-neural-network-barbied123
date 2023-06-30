0f94f10b49ea61e749a1a9679e4ebeff2c4a3309
barbied123 <101971342+barbied123@users.noreply.github.com>
2023-06-30 21:36:05 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,10 +1,9 @@
-# 在该文件NeuralNetwork类中定义你的模型 
-# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
 
+    
 import os
 
-os.system("sudo pip3 install torch")
-os.system("sudo pip3 install torchvision")
+#os.system("sudo pip3 install torch")
+#os.system("sudo pip3 install torchvision")
 
 import torch
 import torch.nn as nn
@@ -12,10 +11,95 @@ import torch.optim as optim
 import torchvision
 
 from torch.utils.data import DataLoader
-    
 
-class NeuralNetwork(nn.Module):
-    pass
+# 定义VGG16模型
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 10),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+# 训练过程
+def train():
+    batch_size = 256  # 批量训练大小
+    model = My_VGG16() # 创建模型
+    # 将模型放入GPU中
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    # 定义损失函数
+    loss_func = nn.CrossEntropyLoss()
+    # 定义优化器
+    optimizer = optim.Adam(params=model.parameters(), lr=0.0002)
+    # 加载数据
+    train_loader = DataLoader(batch_size, shuffle=True)
+    # 训练20次
+    for i in range(20):
+        loss_temp = 0  # 临时变量
+        for j,(batch_data,batch_label) in enumerate(train_loader):
+            # 数据放入GPU中
+            batch_data,batch_label = batch_data.cuda(),batch_label.cuda()
+            # 梯度清零
+            optimizer.zero_grad()
+            # 模型训练
+            prediction = model(batch_data)
+            # 损失值
+            loss = loss_func(prediction,batch_label)
+            loss_temp += loss.item()
+            # 反向传播
+            loss.backward()
+            # 梯度更新
+            optimizer.step()
+        # 打印一次损失值
+        print('[%d] loss: %.3f' % (i+1,loss_temp/len(train_loader)))
+
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
@@ -27,9 +111,13 @@ def read_data():
     return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
-    model = NeuralNetwork() # 若有参数则传入参数
+    model = VGG16() # 若有参数则传入参数
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
+    torch.save(model.state_dict(), 'model.pth')
     model.load_state_dict(torch.load(parent_dir + '/pth/model.pth'))
+    model.train()
     return model
-    
+
+if __name__ == '__main__':
+    main()
